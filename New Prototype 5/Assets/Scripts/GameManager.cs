5babaecdbe6d6ac503e1e72144aa9f7880e64f22
SnubStumpy2007 <joshua192007@gmail.com>
2025-05-09 20:03:30 -0400

    working on game
    Lesson 5.1 complete

--- New Prototype 5/Assets/Scripts/GameManager.cs
+++ New Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,20 +1,32 @@
 using NUnit.Framework;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
 
-    public List<GameObject> targets;
+    public List<GameObject> Targets;
+    private float SpawnRate = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true) { 
+            yield return new WaitForSeconds(SpawnRate);
+            int index = Random.Range(0, Targets.Count);
+            Instantiate(Targets[index]);
+        }
     }
 }
